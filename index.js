b29a8c9020fe16110743d280becf650c62d4f9cf
stratis-vip <stratis.vip@gmail.com>
2021-09-15 21:14:08 +0300

    added

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 "use strict";
-exports.__esModule = true;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.TcxFile = void 0;
-var tcx_file_1 = require("./build/src/classes/tcx-file");
-exports.TcxFile = tcx_file_1["default"];
+var tcx_file_1 = __importDefault(require("./build/src/classes/tcx-file"));
+exports.TcxFile = tcx_file_1.default;
+//# sourceMappingURL=index.js.map
